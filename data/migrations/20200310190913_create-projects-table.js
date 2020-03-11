6193505bb648033c378f97f3c65700488b06b299
josegocampo <jose.ocampo@mail.udp.cl>
2020-03-11 20:21:23 -0300

    MVP done

--- data/migrations/20200310190913_create-projects-table.js
+++ data/migrations/20200310190913_create-projects-table.js
@@ -1,37 +1,37 @@
+exports.up = async function(knex) {
+	await knex.schema.createTable("projects", (table) => {
+		table.increments("id")
+		table.text("name").notNull()
+        table.text("description")
+        table.boolean('completed').default('false')
+	})
 
-exports.up = async function (knex) {
-  await knex.schema.createTable("projects", (table) =>{
-    table.increments("id")
-    table.text("name").notNull().unique()
-    table.text("description")
-    table.boolean("completed").defaultTo("false")  //probably not written this way!!!!
-  })
+	await knex.schema.createTable("tasks", (table) => {
+		table.increments("id")
+        table.text("description").notNull()
+        table.text('notes').notNull()
+        table.boolean('completed').default('false')
+        table.integer('project_id').references('id').inTable('projects')
+	})
 
-  await knex.schema.createTable("resources", (table) => {
-      table.increments("id")
-      table.text("name").notNull().unique()
-      table.text("description")
-  })
+	await knex.schema.createTable("resources", (table) => {
+		table.increments("id")
+        table.text("name").notNull()
+        table.text('description')
 
-  await knex.schema.createTable("tasks", (table) =>{
-      table.increments("id")
-      table.text("description").notNull()
-      table.text("notes")
-      table.boolean("completed").defaultTo("false")
-      table.integer("project_id").references("id").inTable("projects")
-  })
+	})
+	
+	await knex.schema.createTable("project_resources", (table) => {
+		table.increments("id")
+        table.integer("resource_id").references('id').inTable('resources')
+        table.integer('project_id').references('id').inTable('projects')
+	})
 
-//   await knex.schema.createTable("resource_projects", (table) =>{
-//       table.increments("id")
-//   })
+}
 
-};
-
-exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists("tasks")
-  await knex.schema.dropTableIfExists("resources")
-  await knex.schema.dropTableIfExists("projects")
-};
-
-
-exports.up
+exports.down = async function(knex) {
+	await knex.schema.dropTableIfExists("project_resources")
+	await knex.schema.dropTableIfExists("resources")
+	await knex.schema.dropTableIfExists("tasks")
+	await knex.schema.dropTableIfExists("projects")
+}
